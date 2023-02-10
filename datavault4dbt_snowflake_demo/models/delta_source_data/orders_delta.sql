999ab92aa76f8ad108e9b4fc29ba8a9ee6729741
tkirschke <tkirschke@scalefree.com>
2023-02-10 17:30:25 +0100

    initial work on turbovault webinar env

--- datavault4dbt_snowflake_demo/models/delta_source_data/orders_delta.sql
+++
@@ -1,3 +0,0 @@
-{{ config(materialized='view', schema='source_data') }}
-
-SELECT * FROM {{ source('deltas', 'orders_source_data') }}
--- datavault4dbt_snowflake_demo/models/delta_source_data/orders_initial.sql
+++
@@ -1,3 +0,0 @@
-{{ config(materialized='view', schema='source_data') }}
-
-SELECT * FROM {{ ref('orders_source_data') }} WHERE edwLoadDate = TO_TIMESTAMP('2022-10-01T07:00:00', 'YYYY-MM-DDTHH24:MI:SS')
