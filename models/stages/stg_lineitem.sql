72f2f4d411f6cefea882dc8344f3710a628c3db8
Tim Kirschke <81677440+tkirschke@users.noreply.github.com>
2025-03-31 09:52:54 +1100

    switched all stage models to new yaml_metadata syntax

--- models/stages/stg_lineitem.sql
+++ models/stages/stg_lineitem.sql
@@ -34,15 +34,6 @@ ldts: "SYSDATE()"
 rsrc: '!TPC_H_SF1.LineItem'
 {%- endset -%}
 
-{%- set metadata_dict = fromyaml(yaml_metadata) -%}
-
-{{ datavault4dbt.stage(source_model=metadata_dict['source_model'],
-                    ldts=metadata_dict['ldts'],
-                    rsrc=metadata_dict['rsrc'],
-                    hashed_columns=metadata_dict['hashed_columns'],
-                    derived_columns=none,
-                    missing_columns=none,
-                    prejoined_columns=none,
-                    include_source_columns=true) }}
+{{ datavault4dbt.stage(yaml_metadata=yaml_metadata) }}
 
                     
